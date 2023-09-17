e4aa27501fea929574925035bbca9a05ac5ad8d9
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-09-17 11:12:08 -0700

    Add setup script for dev

---
+++ setup.cmd
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+
+set "BROWSER_TAMER_ROOT=%~dp0"
+set "VCPKG_ROOT=%BROWSER_TAMER_ROOT%.build\vcpkg"
+if exist "%VCPKG_ROOT%\bootstrap-vcpkg.bat" goto:$Bootstrap
+git clone https://github.com/Microsoft/vcpkg.git "%VCPKG_ROOT%"
+
+:$Bootstrap
+cd /d "%VCPKG_ROOT%"
+call "%VCPKG_ROOT%\bootstrap-vcpkg.bat"
+vcpkg integrate install
+
+set "variable=E:\myfiles\app1\data\*.csv"
+set "variable=%variable:\=/%"
+echo "%variable%"
+
+endlocal & (
+    set "VCPKG_DISABLE_METRICS=1"
+    set "VCPKG_ROOT=%VCPKG_ROOT:\=/%"
+    set "BROWSER_TAMER_ROOT=%BROWSER_TAMER_ROOT%"
+)
+
+pwsh -NoProfile -File "%BROWSER_TAMER_ROOT%\local-build.ps1"
