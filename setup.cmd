a21eced25a78639cf599a5b4ad91463adde7dd55
joelvaneenwyk <joel.vaneenwyk@gmail.com>
2023-09-19 19:27:17 -0700

    Fix some issues in setup script

--- setup.cmd
+++ setup.cmd
@@ -1,35 +1,14 @@
 @echo off
-setlocal EnableDelayedExpansion
+goto:$Main
 
-set "BROWSER_TAMER_ROOT=%~dp0"
-set "VCPKG_ROOT=%BROWSER_TAMER_ROOT%.build\vcpkg"
-
-git -C "%BROWSER_TAMER_ROOT%" submodule update --init --recursive
-
-call :ReplaceSubmodule "common" "https://github.com/aloneguid/common" "master"
-call :ReplaceSubmodule "grey" "https://github.com/aloneguid/grey" "master"
-call :ReplaceSubmodule "bt/ext/lsignal" "https://github.com/cpp11nullptr/lsignal" "master"
-exit /b
-
-if exist "%VCPKG_ROOT%\bootstrap-vcpkg.bat" goto:$Bootstrap
-git clone "https://github.com/Microsoft/vcpkg.git" "%VCPKG_ROOT%"
-
-:$Bootstrap
-cd /d "%VCPKG_ROOT%"
-call "%VCPKG_ROOT%\bootstrap-vcpkg.bat"
-vcpkg integrate install
-
-set "variable=E:\myfiles\app1\data\*.csv"
-set "variable=%variable:\=/%"
-echo "%variable%"
-
-endlocal & (
-    set "VCPKG_DISABLE_METRICS=1"
-    set "VCPKG_ROOT=%VCPKG_ROOT:\=/%"
-    set "BROWSER_TAMER_ROOT=%BROWSER_TAMER_ROOT%"
-)
-
-call powershell -NoProfile -File "%BROWSER_TAMER_ROOT%\local-build.ps1"
+:Command
+    setlocal EnableDelayedExpansion
+    set "_command=%*"
+    set "_command=!_command:   = !"
+    set "_command=!_command:  = !"
+    echo ##[cmd] !_command!
+    set "PATHEXT=.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC"
+    call !_command!
 exit /b
 
 :ReplaceSubmodule
@@ -54,11 +33,32 @@ exit /b
     call :Command git subtree add --prefix "%new_path%" "%submodule_url%" "%branch%"
 exit /b
 
-:Command
+::
+:: Main Module
+::
+:$Main
     setlocal EnableDelayedExpansion
-    set "_command=%*"
-    set "_command=!_command:   = !"
-    set "_command=!_command:  = !"
-    echo [##cmd] !_command!
-    !_command!
-exit /b
+    set "BROWSER_TAMER_ROOT=%~dp0"
+    set "VCPKG_ROOT=%BROWSER_TAMER_ROOT%.build\vcpkg"
+    if "%BROWSER_TAMER_ROOT:~-1%"=="\" set "BROWSER_TAMER_ROOT=!BROWSER_TAMER_ROOT:~0,-1!"
+
+    call :Command git -C "%BROWSER_TAMER_ROOT%" submodule update --init --recursive
+
+    call :ReplaceSubmodule "common" "https://github.com/aloneguid/common" "master"
+    call :ReplaceSubmodule "grey" "https://github.com/aloneguid/grey" "master"
+    call :ReplaceSubmodule "bt/ext/lsignal" "https://github.com/cpp11nullptr/lsignal" "master"
+    exit /b
+
+    if exist "%VCPKG_ROOT%\bootstrap-vcpkg.bat" goto:$Bootstrap
+    call :Command git clone "https://github.com/Microsoft/vcpkg.git" "%VCPKG_ROOT%"
+
+    :$Bootstrap
+    cd /d "%VCPKG_ROOT%"
+    call "%VCPKG_ROOT%\bootstrap-vcpkg.bat"
+    call :Command vcpkg integrate install
+endlocal & (
+    set "VCPKG_DISABLE_METRICS=1"
+    set "VCPKG_ROOT=%VCPKG_ROOT:\=/%"
+    set "BROWSER_TAMER_ROOT=%BROWSER_TAMER_ROOT%"
+)
+call powershell -NoProfile -File "%BROWSER_TAMER_ROOT%\local-build.ps1"
